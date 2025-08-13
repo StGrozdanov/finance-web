f564094a18bfceb2199af63dba6f76f022664b0e
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2025-08-13 14:40:00 +0300

    home page

--- src/app/(auth)/(home)/page.tsx
+++ src/app/(auth)/(home)/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
+    <article className='flex flex-col items-center justify-center h-screen'>
       <h1 className='text-4xl font-bold'>
-        Letzzz digitally finance those stuffffff
+        Lets digitally finance those stuff !!
       </h1>
-    </div>
+    </article>
   );
 }
